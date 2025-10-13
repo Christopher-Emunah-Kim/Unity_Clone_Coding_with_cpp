3d4173a40fddd3266155167d14f6bd0fce0006de
Christopher-Emunah-Kim <hashin.chris@gmail.com>
2025-10-14 00:43:49 +0900

    <feat> add LoadResource global function

---
+++ Engine_Window/K_LoadResources.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "../SOURCE/K_ResourceTable.h"
+#include "../SOURCE/K_Texture.h"
+
+namespace KHS
+{
+	void LoadResources()
+	{
+		ResourceTable::Load<Texture>(L"BG", L"CloudOcean.png");
+	}
+}
