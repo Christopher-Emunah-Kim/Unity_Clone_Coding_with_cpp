d70ba2adffd0dfa0435ee43565f770019a41e52e
Christopher-Emunah-Kim <hashin.chris@gmail.com>
2025-10-14 00:42:39 +0900

    <feat> ResourceTable 추가
    Resource경로에서 데이터로드용

--- SOURCE/K_Resource.h
+++ SOURCE/K_Resource.h
@@ -7,6 +7,7 @@ namespace KHS
 	class Resource abstract : public Entity
 	{
 	public:
+		Resource() = delete;
 		Resource(EResourceType type)
 			: m_type(type) , m_path(L"")
 		{
@@ -15,7 +16,8 @@ namespace KHS
 
 		virtual ~Resource() {}
 
-		virtual HRESULT Load(const std::wstring& path) = 0;
+		//HRESULT : 0 is S_OK, negative value is fail, positive value is warning
+		virtual HRESULT Load(const std::wstring& path) = 0; 
 		
 		inline const std::wstring& GetPath() { return m_path; }
 		inline void SetPath(const std::wstring& path) { m_path = path; }
---
+++ SOURCE/K_ResourceTable.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include "K_Resource.h"
+
+namespace KHS
+{
+	const wstring DEFAULT_RESOURCE_PATH = L"..\\Resources\\";
+
+	class ResourceTable
+	{
+		
+	public:
+		template<typename T>
+		static T* Find(const std::wstring& key)
+		{
+			auto iter = m_resouces.find(key);
+			if (iter == m_resources.end())
+				return nullptr;
+
+			return dynamic_cast< T* >( iter->second );
+		}
+
+		template<typename T>
+		static T* Load(const std::wstring& key, const std::wstring& path)
+		{
+			std::wstring combinedPath = DEFAULT_RESOURCE_PATH + path;
+
+			T* resource = ResourceTable::Find<T>(key);
+
+			if ( resource != nullptr )
+			{
+				return resource;
+			}
+
+			resource = new T();
+
+			if (FAILED(resource->Load(combinedPath)))
+			{
+				delete resource;
+				assert(false);
+				return nullptr;
+			}
+
+			resource->SetName(key);
+			resource->SetPath(combinedPath);
+			m_resources.insert(std::make_pair(key, resource));
+
+			return resource;
+		}
+
+
+	private:
+		static std::map<std::wstring, Resource*> m_resources;
+	};
+}
